557d97afa8e8433181489960f0ba77a734319f5d
Domodiak <kkv84271@gmail.com>
2023-04-27 22:34:59 +0300

    One more test for Forms

--- Client/src/components/Dropdown/Dropdown.test.js
+++ Client/src/components/Dropdown/Dropdown.test.js
@@ -1,89 +1,91 @@
 import { render, screen, act } from '@testing-library/react';
 import { DropdownMenu } from './Dropdown';
 
-test('renders options correctly', () => {
-    render(<DropdownMenu options={[{label: "A", value: "a"},
-        {label: "B", value: "b"}
-    ]} dropdownClass="TestDropdown" visible />);
-    var optionA = screen.getByText("A")
-    var optionB = screen.getByText("B")
-    expect(optionA).toBeInTheDocument();
-    expect(optionB).toBeInTheDocument();
-});
-
-test('handles clicks correctly', () => {
-    const handleSelect = jest.fn()
-    var options = [
-        {label: "A", value: "a"},
-        {label: "B", value: "b"}
-    ]
-
-    render(<DropdownMenu options={options} dropdownClass="TestDropdown" visible select={handleSelect} />);
-    var optionA = screen.getByText("A")
-    var optionB = screen.getByText("B")
+describe('Dropdown', () => {
+    test('renders options correctly', () => {
+        render(<DropdownMenu options={[{label: "A", value: "a"},
+            {label: "B", value: "b"}
+        ]} dropdownClass="TestDropdown" visible />);
+        var optionA = screen.getByText("A")
+        var optionB = screen.getByText("B")
+        expect(optionA).toBeInTheDocument();
+        expect(optionB).toBeInTheDocument();
+    });
     
-    act(() => {
-        optionA.click()
+    test('handles clicks correctly', () => {
+        const handleSelect = jest.fn()
+        var options = [
+            {label: "A", value: "a"},
+            {label: "B", value: "b"}
+        ]
+    
+        render(<DropdownMenu options={options} dropdownClass="TestDropdown" visible select={handleSelect} />);
+        var optionA = screen.getByText("A")
+        var optionB = screen.getByText("B")
+        
+        act(() => {
+            optionA.click()
+        })
+        expect(handleSelect).toHaveBeenCalledTimes(1)
+        expect(handleSelect).toHaveBeenCalledWith("a")
+    
+        act(() => {
+            optionB.click()
+        })
+        expect(handleSelect).toHaveBeenCalledWith("b")
+    
+        act(() => {
+            optionB.click()
+        })
+        expect(handleSelect).toHaveBeenCalledWith("b")
+    });
+    
+    test('hides when something was chosen', () => {
+        const handleSelect = jest.fn();
+        const setVisible = jest.fn();
+      
+        const { getByTestId } = render(
+            <DropdownMenu
+                options={[
+                    { label: 'A', value: 'a' },
+                    { label: 'B', value: 'b' },
+                ]}
+                dropdownClass="TestDropdown"
+                visible={true}
+                setVisible={setVisible}
+                closeOnChoose={true}
+                select={handleSelect}
+            />
+        );
+      
+        const optionA = screen.getByText('A');
+        const dropdownElement = getByTestId('dropdown');
+      
+        expect(dropdownElement).toBeInTheDocument();
+        expect(optionA).toBeInTheDocument();
+    
+        act(() => {
+            optionA.click();
+        })
+      
+        expect(setVisible).toHaveBeenCalledWith(false);
+        expect(handleSelect).toHaveBeenCalledWith('a');
+    });
+    
+    test("hides when clicked away", () => {
+        const setVisible = jest.fn()
+        const handleSelect = jest.fn()
+    
+        render(
+            <DropdownMenu options={[{label: "A", value: "a"},
+               {label: "B", value: "b"}
+            ]} dropdownClass="TestDropdown" visible setVisible={setVisible} closeOnFocusLost select={handleSelect} />
+        );
+    
+        document.dispatchEvent(new MouseEvent('mouseup', { bubbles: true }))
+    
+        expect(setVisible).toHaveBeenCalledTimes(1)
+        expect(setVisible).toHaveBeenCalledWith(false)
+        expect(handleSelect).toHaveBeenCalledTimes(0)
     })
-    expect(handleSelect).toHaveBeenCalledTimes(1)
-    expect(handleSelect).toHaveBeenCalledWith("a")
-
-    act(() => {
-        optionB.click()
-    })
-    expect(handleSelect).toHaveBeenCalledWith("b")
-
-    act(() => {
-        optionB.click()
-    })
-    expect(handleSelect).toHaveBeenCalledWith("b")
-});
-
-test('hides when something was chosen', () => {
-    const handleSelect = jest.fn();
-    const setVisible = jest.fn();
-  
-    const { getByTestId } = render(
-        <DropdownMenu
-            options={[
-                { label: 'A', value: 'a' },
-                { label: 'B', value: 'b' },
-            ]}
-            dropdownClass="TestDropdown"
-            visible={true}
-            setVisible={setVisible}
-            closeOnChoose={true}
-            select={handleSelect}
-        />
-    );
-  
-    const optionA = screen.getByText('A');
-    const dropdownElement = getByTestId('dropdown');
-  
-    expect(dropdownElement).toBeInTheDocument();
-    expect(optionA).toBeInTheDocument();
-
-    act(() => {
-        optionA.click();
-    })
-  
-    expect(setVisible).toHaveBeenCalledWith(false);
-    expect(handleSelect).toHaveBeenCalledWith('a');
-});
-
-test("hides when clicked away", () => {
-    const setVisible = jest.fn()
-    const handleSelect = jest.fn()
-
-    render(
-        <DropdownMenu options={[{label: "A", value: "a"},
-           {label: "B", value: "b"}
-        ]} dropdownClass="TestDropdown" visible setVisible={setVisible} closeOnFocusLost select={handleSelect} />
-    );
-
-    document.dispatchEvent(new MouseEvent('mouseup', { bubbles: true }))
-
-    expect(setVisible).toHaveBeenCalledTimes(1)
-    expect(setVisible).toHaveBeenCalledWith(false)
-    expect(handleSelect).toHaveBeenCalledTimes(0)
 })
--- Client/src/components/Forms/Form.test.js
+++ Client/src/components/Forms/Form.test.js
@@ -1,35 +1,65 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { Form } from './Form.jsx'
 import { TextField } from './TextField.jsx'
 import { FormSubmit } from './FormSubmit.jsx'
 
-test("renders contents of the form properly", () => {
-    var input = {}
-    var submitCount = 0
-    const handleInput = (event) => {
-        var name = event.target.getAttribute('name')
-        var value = event.target.value
-
-        input[name] = value
-    }
-
-    const handleSubmit = () => {
-        submitCount += 1
-    }
-
-    render(
-        <Form onSubmit={handleSubmit}>
-            <TextField type='text' onChange={handleInput} placeholder='Username' index='1' required name="username" />
-            <TextField type='password' onChange={handleInput} placeholder='Password' index='2' required name="password" />
-            <FormSubmit content="Log in" index="3"/>
-        </Form>
-    )
+describe('Form', () => {
+    test("renders contents of the form properly", () => {
+        render(
+            <Form>
+                <TextField type='text' onChange={() => {}} placeholder='Username' index='1' required name="username" />
+                <TextField type='password' onChange={() => {}} placeholder='Password' index='2' required name="password" />
+                <FormSubmit content="Log in" index="3"/>
+            </Form>
+        )
+        
+        var usernameField = screen.getByPlaceholderText('Username')
+        var passwordField = screen.getByPlaceholderText('Password')
+        var formSubmit = screen.getByText("Log in")
     
-    var usernameField = screen.getByPlaceholderText('Username')
-    var passwordField = screen.getByPlaceholderText('Password')
-    var formSubmit = screen.getByText("Log in")
+        expect(usernameField).toBeInTheDocument();
+        expect(passwordField).toBeInTheDocument();
+        expect(formSubmit).toBeInTheDocument();
+    })
 
-    expect(usernameField).toBeInTheDocument();
-    expect(passwordField).toBeInTheDocument();
-    expect(formSubmit).toBeInTheDocument();
+    test("handles input correctly", () => {
+        var onInput = jest.fn();
+        const handleInput = (event) => {
+            var name = event.target.getAttribute("name");
+            var value = event.target.value;
+            onInput([name, value]);
+        };
+      
+        render(
+          <Form>
+            <TextField
+                type="text"
+                onChange={handleInput}
+                placeholder="Username"
+                index="1"
+                required
+                name="username"
+            />
+            <TextField
+                type="password"
+                onChange={handleInput}
+                placeholder="Password"
+                index="2"
+                required
+                name="password"
+            />
+            <FormSubmit content="Log in" index="3" />
+          </Form>
+        );
+      
+        var usernameInput = screen.getByPlaceholderText("Username");
+      
+        const target = "thisIsAUsername"
+        userEvent.type(usernameInput, target);
+      
+        expect(onInput).toHaveBeenCalledTimes(target.length);
+        expect(onInput).toHaveBeenCalledWith(["username", target]);
+      });
+      
 })
